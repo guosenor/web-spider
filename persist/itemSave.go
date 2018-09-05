dfe70d7dfa4ca524352d91da7188111900a9f355
guosen <guosen@bimsop.com>
2018-09-05 11:52:53 +0800

    saveToElastic

--- persist/itemSave.go
+++ persist/itemSave.go
@@ -1,16 +1,31 @@
 package persist
 
-import "log"
+import (
+	"log"
+	"gopkg.in/olivere/elastic.v5"
+	"context"
+)
 
 func ItemSave() chan interface{} {
 	out := make(chan interface{})
-	itemCount:=0
+	itemCount := 0
 	go func() {
 		for {
-			item := <- out
+			item := <-out
 			itemCount++
-			log.Printf("item save got a item %d -- %v \n",itemCount,item)
+			log.Printf("item save got a item %d -- %v \n", itemCount, item)
 		}
 	}()
 	return out
+}
+func saveToElastic(item interface{}) (id string, err error) {
+	client, err := elastic.NewClient(elastic.SetSniff(false))
+	if err != nil {
+		return "", err
+	}
+	res, err := client.Index().Index("dating_profile").Type("zhenai").BodyJson(item).Do(context.Background())
+	if err != nil {
+		return "",err
+	}
+	return res.Id,nil
 }
---
+++ persist/itemSave_test.go
@@ -0,0 +1,39 @@
+package persist
+
+import (
+	"testing"
+	"github.com/guosenor/web-spider/model"
+	"gopkg.in/olivere/elastic.v5"
+	"context"
+	"encoding/json"
+)
+
+func TestSaveToElastic(t *testing.T) {
+	item := model.UserProfile{
+		Name:"guosen",
+		Gender:"男",
+		Age:18,
+	}
+	id,err := saveToElastic(item)
+	if err!= nil {
+		panic(err)
+	}
+	client, err := elastic.NewClient(elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+	res,err := client.Get().Index("dating_profile").Type("zhenai").Id(id).Do(context.Background())
+	if err!=nil {
+		panic(err)
+	}
+	var actual model.UserProfile
+	err = json.Unmarshal(*res.Source, &actual)
+	if err !=nil {
+		panic(err)
+	}
+
+	if item != actual {
+		t.Errorf("got %v item %v" ,item,actual)
+	}
+
+}
